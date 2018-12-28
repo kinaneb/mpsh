560a884351d82b9e273f4175a139a33f82fdbf74
thafath <thafathboudjerda1988@gmail.com>
2018-12-28 12:58:01 +0100

    les functions .h et .c

---
+++ mpsh_func.h
@@ -0,0 +1,11 @@
+#ifndef MPSH_H
+#define MPSH_H
+
+
+void echoShell(char *texteUtilisateur);
+void pwdShell(char *dossierType);
+void cdShell(char *pathUtilisateur);
+void catShell(char *pathFichier);
+void lsShell(char *pathDossier);
+
+#endif
