945bffaa5e9d1492a5b4db44dd07fedb443b801c
thafath <thafathboudjerda1988@gmail.com>
2018-12-31 09:50:14 +0100

    le fichier .h de && ||

---
+++ enchaine.h
@@ -0,0 +1,13 @@
+#ifndef ENCHAINE_H
+#define ENCHAINE_H
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+
+void Enchaine_OU(char *s);
+void Enchaine_ET(char *s);
+#endif
